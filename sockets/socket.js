c7755b76d4dc7b7f87b4fbee77f98d0cc173dbb3
Ikechi Michael <mykehell123@gmail.com>
2017-10-21 00:31:28 +0100

    receive time logs per user

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,34 @@
 module.exports = (app) => {
     require('express-ws')(app)
 
-    app.ws('/', (ws, req) => {
-        ws.on('message', (message) => {
-            console.log(message);
+    app.ws('/time', (ws, req) => {
+        const usersTime = {}
+
+        ws.on('message', (message = '') => {
+            try {
+                const data = JSON.parse(message)
+                /**
+                 * {
+                 *  "id": 1
+                 * }
+                 */
+                if (data.constructor.name === 'Object') {
+                    if (!!Number(data.id)) { //check that [id] is a number
+                        usersTime[Number(data.id)] = (usersTime[Number(data.id)] || 0) + 10 
+                        ws.send(usersTime[Number(data.id)])
+                    }
+                    else {
+                        console.error("[data.id] is NaN", data.id)
+                    }
+                }
+                else {
+                    console.error('[data] is not an Object', data)
+                }
+            }
+            catch (ex) {
+                console.error("there was an error parsing", message)
+            }
+            //console.log(message);
         })
     })
 }

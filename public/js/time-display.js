2cc3a7c4957b541297adef0e243b7a0fb9e71fde
Ikechi Michael <mykehell123@gmail.com>
2017-10-21 09:21:49 +0100

    added computed properties and methods

--- public/js/time-display.js
+++ public/js/time-display.js
@@ -1,5 +1,23 @@
 /// <reference path="./min/vue.min.js" />
 
 const TimeDisplay = Vue.component('time-display', {
-    props: ['seconds']
+    props: ['seconds'],
+    computed: {
+        hours() {
+            return this.pad(Math.floor(this.seconds / 3600), 2)
+        },
+        minutes() {
+            return this.pad(Math.floor(this.seconds / 60), 2)
+        },
+        time() {
+            return `${this.hours} : ${this.minutes} : ${this.pad(this.seconds % 60, 2)}`;
+        }
+    },
+    methods: {
+        pad (num, count) {
+            count = count || 0;
+            const $num = num + '';
+            return '0'.repeat(count - $num.length) + $num;
+        }
+    }
 })

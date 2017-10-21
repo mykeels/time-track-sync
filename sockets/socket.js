31fb68d7ba3cdf2cb2bc9e96f4f47cf5d2625e2e
Ikechi Michael <mykehell123@gmail.com>
2017-10-21 09:22:09 +0100

    added refresh endpoint

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
     require('express-ws')(app)
 
-    app.ws('/time', (ws, req) => {
-        const usersTime = {}
+    const usersTime = {}
 
+    app.ws('/time', (ws, req) => {
         ws.on('message', (message = '') => {
             try {
                 const data = JSON.parse(message)
@@ -14,8 +14,13 @@ module.exports = (app) => {
                  */
                 if (data.constructor.name === 'Object') {
                     if (!!Number(data.id)) { //check that [id] is a number
-                        usersTime[Number(data.id)] = (usersTime[Number(data.id)] || 0) + 10 
-                        ws.send(usersTime[Number(data.id)])
+                        if (data.message === 'update') {
+                            usersTime[Number(data.id)] = (usersTime[Number(data.id)] || 0) + 10 
+                            ws.send(usersTime[Number(data.id)])
+                        }
+                        else if (data.message === 'refresh') {
+                            ws.send(usersTime[Number(data.id)])
+                        }
                     }
                     else {
                         console.error("[data.id] is NaN", data.id)

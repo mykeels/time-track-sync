5fc9b7dad788242ed26deb915bf919992c5482ba
Ikechi Michael <mykehell123@gmail.com>
2017-10-24 15:03:10 +0100

    set inactivity tracker to stop on page-leave

--- public/js/inactivity-tracker.js
+++ public/js/inactivity-tracker.js
@@ -1,57 +1,71 @@
-const InactivityTracker = Vue.component('inactivity-tracker', {
-    data() {
-        return {
-            startTime: new Date(),
-            endTime: new Date()
-        }
+const InactivityTracker = Vue.component("inactivity-tracker", {
+  data() {
+    return {
+      startTime: new Date(),
+      endTime: new Date()
+    };
+  },
+  methods: {
+    pad(num, count) {
+      num = Math.floor(num);
+      count = count || Number.POSITIVE_INFINITY;
+      const $num = num + "";
+      return "0".repeat(count - $num.length) + $num;
     },
-    methods: {
-        pad (num, count) {
-            num = Math.floor(num);
-            count = count || Number.POSITIVE_INFINITY;
-            const $num = num + '';
-            return '0'.repeat(count - $num.length) + $num;
-        },
-        start() {
-            if (this.interval) clearInterval(this.interval);
-            this.interval = setInterval((function () {
-                this.endTime = new Date();
-                const ALERT_INTERVAL = 60;
-                if (this.seconds >= ALERT_INTERVAL) {
-                    alert(`${ALERT_INTERVAL} seconds have elapsed since your last activity`)
-                    this.reset();
-                    this.$parent.$emit('stop');
-                }
-            }).bind(this), 1000)
-        },
-        stop() {
-            clearInterval(this.interval)
-        },
-        reset() {
-            this.startTime = this.endTime;
-            //console.log("inactivity-tracker-reset");
-        }
+    start() {
+      if (this.interval) clearInterval(this.interval);
+      this.interval = setInterval(
+        function() {
+          this.endTime = new Date();
+          const ALERT_INTERVAL = 10;
+          if (this.seconds >= ALERT_INTERVAL) {
+            this.stop();
+            this.$parent.$emit("stop");
+            alert(
+              `${ALERT_INTERVAL} seconds have elapsed since your last activity`
+            );
+            this.start();
+            this.reset();
+          }
+        }.bind(this),
+        1000
+      );
     },
-    computed: {
-        elapsed() {
-            return this.endTime - this.startTime;
-        },
-        hours() {
-            return this.pad(Math.floor((this.elapsed / 1000) / 3600), 2)
-        },
-        minutes() {
-            return this.pad(Math.floor(((this.elapsed / 1000) - (this.hours * 3600)) / 60), 2)
-        },
-        seconds() {
-            return this.pad((this.elapsed / 1000) - (this.minutes * 60) - (this.hours * 3600), 2)
-        },
-        time() {
-            return `${this.hours} : ${this.minutes} : ${this.seconds}`;
-        }
+    stop() {
+      clearInterval(this.interval);
     },
-    mounted() {
-        //this.start()
-
-        this.$parent.$on('reset', this.reset.bind(this))
+    reset() {
+      this.startTime = this.endTime;
     }
-})
+  },
+  computed: {
+    elapsed() {
+      return this.endTime - this.startTime;
+    },
+    hours() {
+      return this.pad(Math.floor(this.elapsed / 1000 / 3600), 2);
+    },
+    minutes() {
+      return this.pad(
+        Math.floor((this.elapsed / 1000 - this.hours * 3600) / 60),
+        2
+      );
+    },
+    seconds() {
+      return this.pad(
+        this.elapsed / 1000 - this.minutes * 60 - this.hours * 3600,
+        2
+      );
+    },
+    time() {
+      return `${this.hours} : ${this.minutes} : ${this.seconds}`;
+    }
+  },
+  mounted() {
+    this.start();
+
+    this.$parent.$on("reset", this.reset.bind(this));
+    this.$parent.$on("stop", this.stop.bind(this));
+    this.$parent.$on("start", this.start.bind(this));
+  }
+});
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,82 +1,96 @@
-module.exports = (app) => {
-    require('express-ws')(app)
+module.exports = app => {
+  require("express-ws")(app);
 
-    const usersTime = {}
+  const usersTime = {};
 
-    app.ws('/time', (ws, req) => {
-        ws.on('message', (message = '') => {
-            try {
-                const data = JSON.parse(message)
-                /**
+  app.ws("/time", (ws, req) => {
+    ws.on("message", (message = "") => {
+      try {
+        const data = JSON.parse(message);
+        /**
                  * {
                  *  "id": 1,
                  *  "patientId": 874
                  * }
                  */
-                if (data.constructor.name === 'Object') {
-                    if ((!!Number(data.id) || Number(data.id) === 0) &&
-                        (!!Number(data.patientId) || Number(data.patientId) === 0)) { //check that [id] and [patientId] are numbers
-                        const key = `${data.id}-${data.patientId}`;
-                        ws.key = key;
-                        if (data.message === 'update') {
-                            usersTime[key] = usersTime[key] || {
-                                date: new Date(),
-                                sockets: [],
-                                interval() {
-                                    return Math.floor(((new Date()) - this.date) / 1000)
-                                }
-                            }
-                            if (usersTime[key].sockets.indexOf(ws) < 0) { //add ws to client sockets list
-                                usersTime[key].sockets.push(ws)
-                            }
-                            ws.send(JSON.stringify({
-                                seconds: usersTime[key].interval(),
-                                clients: usersTime[key].sockets.length
-                            }))
-                        }
-                        else if (data.message === 'stop') {
-                            ws.clientState = 'stopped';
-                            ws.send(JSON.stringify({
-                                message: 'ws stopped'
-                            }))
-                        }
-                        else if (data.message === 'start') {
-                            ws.clientState = null;
-                            ws.send(JSON.stringify({
-                                message: 'ws started'
-                            }))
-                        }
-                    }
-                    else {
-                        console.error("[data.id] is NaN", data.id)
-                    }
+        if (data.constructor.name === "Object") {
+          if (
+            (!!Number(data.id) || Number(data.id) === 0) &&
+            (!!Number(data.patientId) || Number(data.patientId) === 0)
+          ) {
+            //check that [id] and [patientId] are numbers
+            const key = `${data.id}-${data.patientId}`;
+            ws.key = key;
+            if (data.message === "update") {
+              usersTime[key] = usersTime[key] || {
+                date: new Date(),
+                sockets: [],
+                interval() {
+                  return Math.floor((new Date() - this.date) / 1000);
                 }
-                else {
-                    console.error('[data] is not an Object', data)
-                }
+              };
+              if (usersTime[key].sockets.indexOf(ws) < 0) {
+                //add ws to client sockets list
+                usersTime[key].sockets.push(ws);
+              }
+              ws.send(
+                JSON.stringify({
+                  seconds: usersTime[key].interval(),
+                  clients: usersTime[key].sockets.length
+                })
+              );
+            } else if (data.message === "stop") {
+              ws.clientState = "stopped";
+              ws.send(
+                JSON.stringify({
+                  message: "ws stopped"
+                })
+              );
+            } else if (data.message === "start") {
+              ws.clientState = null;
+              ws.send(
+                JSON.stringify({
+                  message: "ws started"
+                })
+              );
             }
-            catch (ex) {
-                console.error("there was an error parsing", message)
-            }
-        })
-
-        ws.on('close', (ev) => {
-            const key = ws.key;
-            if (key) usersTime[key].sockets.splice(usersTime[key].sockets.indexOf(ws), 1);
-        })
-    })
-
-    setInterval(() => {
-        for (const key in usersTime) {
-            console.log("sending message to ", usersTime[key].sockets.length, " clients");
-            usersTime[key].sockets.forEach(socket => {
-                if (socket.clientState != 'stopped') {
-                    socket.send(JSON.stringify({
-                        seconds: usersTime[key].interval(),
-                        clients: usersTime[key].sockets.length
-                    }))
-                }
-            })
+          } else {
+            console.error("[data.id] is NaN", data.id);
+          }
+        } else {
+          console.error("[data] is not an Object", data);
         }
-    }, 3000)
-}
+      } catch (ex) {
+        console.error("there was an error parsing", message);
+      }
+    });
+
+    ws.on("close", ev => {
+      const key = ws.key;
+      if (key)
+        usersTime[key].sockets.splice(usersTime[key].sockets.indexOf(ws), 1);
+    });
+  });
+
+  setInterval(() => {
+    for (const key in usersTime) {
+      console.log(
+        "sending message to ",
+        usersTime[key].sockets.filter(
+          socket => socket.clientState !== "stopped"
+        ).length,
+        " clients"
+      );
+      usersTime[key].sockets.forEach(socket => {
+        if (socket.clientState != "stopped") {
+          socket.send(
+            JSON.stringify({
+              seconds: usersTime[key].interval(),
+              clients: usersTime[key].sockets.length
+            })
+          );
+        }
+      });
+    }
+  }, 3000);
+};
